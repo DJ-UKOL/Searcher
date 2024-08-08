67b28849800704fb88d496b3dfbe1aa99291a6f1
Admin <dj-ukol@yandex.ru>
2024-08-08 13:18:28 +0500

    Add function get_input()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::Write;
+
+// Функция получения ввода от пользователя
+fn get_input(query: &str) -> std::io::Result<String> {
+    print!("{}", query);
+    std::io::stdout().flush()?;
+
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer)?;
+
+    Ok(buffer.trim().to_owned())
+}
+
 fn main() {
     println!("Hello, world!");
 }
